5f3040e6072d25fb610ea44cb7da5f760668f8c5
Moritz Fischer <moritz.fischer@student.kit.edu>
2011-10-17 00:37:26 +0200

    Cleaned up Test.hs furthermore

--- Test.hs
+++ Test.hs
@@ -14,22 +14,22 @@ import Lowlevel.PCSCLite ( SCardScope (UserScope)
 
 import Control.Monad
 
-tryConnection c (r:rs) = do putStrLn $"Found readers: " ++ show (r:rs)
-                            x <- connect c r Shared [T0, T1]
-                            case x of
-                                 Left  err    -> putStrLn $show err
-                                 Right (p, h) -> do { putStrLn $"Connected, Protocol is: " ++ show p
-                                                   ; rt <- transmit h [0xff, 0x00, 0x40, 0x50, 0x04, 0x05, 0x05, 0x03, 0x01] 200 T0
-                                                   ; case rt of
-                                                          Left s  -> print s
-                                                          Right a -> putStrLn $ "Answer is: " ++ show a
-                                                   ; rt' <- status h 200 200
-                                                   ; case rt' of
-                                                          Left  s -> print s
-                                                          Right a -> putStrLn $ "Queriying the status: " ++ show rt'
-                                                   ; rt'' <- listReaderGroups c 200
-                                                   ; putStrLn $ "Listing the reader groups: " ++ show rt''
-                                                   ; return () }
+tryConnection c r'@(r:rs) = do printShow r' "Found readers: "
+                               x <- connect c r Shared [T0, T1]
+                               either print f x
+                               where f (p, h) = do
+                                     printShow p "Connected, Protocol is: "
+                                     rt <- transmit h [0xff, 0x00, 0x40, 0x50, 0x04, 0x05, 0x05, 0x03, 0x01] 200 T0
+                                     either print (`printShow` "Answer is: ") rt
+                                     rt' <- status h 200 200
+                                     printShow rt' "Querying the status: "
+                                     rt'' <- listReaderGroups c 200
+                                     printShow rt'' "Listing the reader groups: "
+                                     return ()
+
+
+printShow :: Show a => a -> String -> IO ()
+printShow s = putStrLn . (flip (++) . show) s
 
 withContext :: SCardScope -> (SCardContext -> IO a) -> IO (Either String SCardStatus)
 withContext s f = establishContext s >>= \r -> either (return . Left . show) (\c -> f c >> liftM Right (releaseContext c)) r
