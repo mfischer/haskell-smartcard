503649f32005687cdc8b1ffda9bd65ace4c5a820
Moritz Fischer <moritz.fischer@student.kit.edu>
2011-10-16 23:59:00 +0200

    Followed hlint's suggestions for Test.hs

--- Test.hs
+++ Test.hs
@@ -21,20 +21,20 @@ tryConnection c (r:rs) = do putStrLn $"Found readers: " ++ show (r:rs)
                                  Right (p, h) -> do { putStrLn $"Connected, Protocol is: " ++ show p
                                                    ; rt <- transmit h [0xff, 0x00, 0x40, 0x50, 0x04, 0x05, 0x05, 0x03, 0x01] 200 T0
                                                    ; case rt of
-                                                          Left s  -> putStrLn $ show s
-                                                          Right a -> putStrLn $ "Answer is: " ++ (show a)
+                                                          Left s  -> print s
+                                                          Right a -> putStrLn $ "Answer is: " ++ show a
                                                    ; rt' <- status h 200 200
                                                    ; case rt' of
-                                                          Left  s -> putStrLn $ show s
-                                                          Right a -> putStrLn $ "Queriying the status: " ++ (show rt')
+                                                          Left  s -> print s
+                                                          Right a -> putStrLn $ "Queriying the status: " ++ show rt'
                                                    ; rt'' <- listReaderGroups c 200
-                                                   ; putStrLn $"Listing the reader groups: " ++ (show rt'')
+                                                   ; putStrLn $ "Listing the reader groups: " ++ show rt''
                                                    ; return () }
 
 withContext :: SCardScope -> (SCardContext -> IO a) -> IO (Either String SCardStatus)
-withContext s f = establishContext s >>= \r -> either (return . Left . show) (\c -> f c >> (liftM Right $ releaseContext c)) r
+withContext s f = establishContext s >>= \r -> either (return . Left . show) (\c -> f c >> liftM Right (releaseContext c)) r
 
 main =  withContext UserScope $ \c ->
           do rs <- listReaders c
-             either (putStrLn . show) (tryConnection c) rs
+             either print (tryConnection c) rs
              return ()
